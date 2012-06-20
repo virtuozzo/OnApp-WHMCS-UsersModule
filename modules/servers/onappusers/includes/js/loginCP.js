fd6acf87f3c857077f09587f3623b3c7cb713e7e
Lev Bartashevsky <devman@iMac.local>
2012-06-20 18:15:34 +0300

    add "Manage My Cloud" button to "Default" theme

--- modules/servers/onappusers/includes/js/loginCP.js
+++ modules/servers/onappusers/includes/js/loginCP.js
@@ -1,11 +1,23 @@
 $( document ).ready( function () {
-	var tbody = $( "td:contains('" + injTarget + "'):last" ).parent().parent();
-	tbody.parent().attr( 'id', 'LoginDetails' );
-	tbody.append( tbody.html() );
+	var loginDetails = $( "td:contains('" + injTarget + "'):last" );
+	if( loginDetails.length ) {
+		var tbody = loginDetails.parent().parent();
+		tbody.parent().attr( 'id', 'LoginDetails' );
+		tbody.append( tbody.html() );
 
-	var server = LANG.onappuserslogintocpserver;
-	$( 'table#LoginDetails tr:last td:first' ).html( server );
+		var server = LANG.onappuserslogintocpserver;
+		$( 'table#LoginDetails tr:last td:first' ).html( server );
 
-	server = '<div id="gotocpserver">' + SERVER + '</div>' + $( '#gotocp' ).html();
-	$( 'table#LoginDetails tr:last td:last' ).html( server );
+		server = '<div id="gotocpserver">' + SERVER + '</div>' + $( '#gotocp' ).html();
+		$( 'table#LoginDetails tr:last td:last' ).html( server );
+	}
+	else {
+		var target = $( 'ul.tabs li:last' );
+		target.before( '<li><a href="#" id="gotocpspan">' + LANG.onappuserslogintocpbutton + '</a></li>' );
+
+		$( '#gotocpspan' ).click( function() {
+			$( '#gotocpform' ).submit();
+			return false;
+		} );
+	}
 } );
