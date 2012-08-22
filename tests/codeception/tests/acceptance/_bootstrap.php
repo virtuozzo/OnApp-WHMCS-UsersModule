962406ff2474fcab2fa080e58b9b7c365e337142
Lev Bartashevsky <devman@iMac.local>
2012-08-22 15:10:03 +0300

    add bootstrap file

---
+++ tests/codeception/tests/acceptance/_bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+// Here you can initialize variables that will for your tests
+
+$server[ 'onappusers' ][ 'name' ] = 'Codeception UserModule Test';
+$server[ 'onappusers' ][ 'type' ] = 'onappusers';
+
+// add your credentials for tests
+$server[ 'onappusers' ][ 'host' ] = '';
+$server[ 'onappusers' ][ 'user' ] = '';
+$server[ 'onappusers' ][ 'pass' ] = '';
+
+$WHMCS[ 'admin' ][ 'user' ] = '';
+$WHMCS[ 'admin' ][ 'pass' ] = '';
