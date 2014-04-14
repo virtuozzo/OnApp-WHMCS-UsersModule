1028393f10720ce789d396b44b0b8f6e19846505
Lev Bartashevsky <n@n>
2014-04-14 16:27:06 +0300

    align code

--- modules/servers/onappusers/includes/php/CURL.php
+++ modules/servers/onappusers/includes/php/CURL.php
@@ -1,129 +1,130 @@
 <?php
 
 class CURL {
-	private $ch;
-	private $data;
-	private $customOptions = array( );
+    private $ch;
+    private $data;
+    private $customOptions  = array();
+    private $defaultOptions = array(
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_USERAGENT      => 'CURL',
+        CURLOPT_HEADER         => false,
+        CURLOPT_NOBODY         => false,
+    );
 
-	private $defaultOptions = array(
-		CURLOPT_SSL_VERIFYPEER => false,
-		CURLOPT_SSL_VERIFYHOST => false,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_USERAGENT => 'CURL',
-		CURLOPT_HEADER => false,
-		CURLOPT_NOBODY => false,
-	);
+    public function __construct() {
+        $this->ch = curl_init();
+    }
 
-	public function __construct( ) {
-		$this->ch = curl_init( );
-	}
+    public function useCookies() {
+        $cookiesFile = tempnam( '/tmp', 'OnApp_CURL_cookies' );
+        $this->defaultOptions[ CURLOPT_COOKIEFILE ] = $cookiesFile;
+        $this->defaultOptions[ CURLOPT_COOKIEJAR ] = $cookiesFile;
+    }
 
-	public function useCookies() {
-		$cookiesFile = tempnam( '/tmp', 'OnApp_CURL_cookies' );
-		$this->defaultOptions[ CURLOPT_COOKIEFILE ] = $cookiesFile;
-		$this->defaultOptions[ CURLOPT_COOKIEJAR ]  = $cookiesFile;
-	}
+    public function addOption( $name, $value ) {
+        $this->customOptions[ $name ] = $value;
+    }
 
-	public function addOption( $name, $value ) {
-		$this->customOptions[ $name ] = $value;
-	}
+    public function setLog() {
+        $log = fopen( __DIR__ . '/CURL.log', 'a' );
+        if( $log ) {
+            fwrite( $log, str_repeat( '=', 80 ) . PHP_EOL );
+            $this->addOption( CURLOPT_STDERR, $log );
+            $this->addOption( CURLOPT_VERBOSE, true );
+        }
+    }
 
-	public function setLog( ) {
-		$log = fopen( dirname( __FILE__ ) . '/CURL.log', 'a' );
-		if( $log ) {
-			fwrite( $log, str_repeat( '=', 80 ) . PHP_EOL );
-			$this->addOption( CURLOPT_STDERR, $log );
-			$this->addOption( CURLOPT_VERBOSE, true );
-		}
-	}
+    public function put( $url = null ) {
+        return $this->send( 'PUT', $url );
+    }
 
-	public function put( $url = null ) {
-		return $this->send( 'PUT', $url );
-	}
+    public function get( $url = null ) {
+        return $this->send( 'GET', $url );
+    }
 
-	public function get( $url = null ) {
-		return $this->send( 'GET', $url );
-	}
+    public function post( $url = null ) {
+        return $this->send( 'POST', $url );
+    }
 
-	public function post( $url = null ) {
-		return $this->send( 'POST', $url );
-	}
+    public function head( $url = null ) {
+        return $this->send( 'HEAD', $url );
+    }
 
-	public function head( $url = null ) {
-		return $this->send( 'HEAD', $url );
-	}
+    public function getRequestInfo( $param = false ) {
+        if( $param ) {
+            return $this->getDataItem( 'info', $param );
+        }
+        else {
+            return $this->data[ 'info' ];
+        }
+    }
 
-	public function getRequestInfo( $param = false ) {
-		if( $param ) {
-			return $this->getDataItem( 'info', $param );
-		}
-		else {
-			return $this->data[ 'info' ];
-		}
-	}
+    public function getHeadersData( $param = false ) {
+        if( $param ) {
+            return $this->getDataItem( 'data', $param );
+        }
 
-	public function getHeadersData( $param = false ) {
-		if( $param ) {
-			return $this->getDataItem( 'data', $param );
-		}
-		return $this->data[ 'data' ];
-	}
+        return $this->data[ 'data' ];
+    }
 
-	private function send( $method, $url ) {
-		if( $url === null ) {
-			if( !isset( $this->customOptions[ CURLOPT_URL ] ) || empty( $this->customOptions[ CURLOPT_URL ] ) ) {
-				exit( 'empty url' );
-			}
-		}
-		$this->addOption( CURLOPT_CUSTOMREQUEST, $method );
-		$this->addOption( CURLOPT_URL, $url );
-		return $this->exec( );
-	}
+    private function send( $method, $url ) {
+        if( $url === null ) {
+            if( ! isset( $this->customOptions[ CURLOPT_URL ] ) || empty( $this->customOptions[ CURLOPT_URL ] ) ) {
+                exit( 'empty url' );
+            }
+        }
+        $this->addOption( CURLOPT_CUSTOMREQUEST, $method );
+        $this->addOption( CURLOPT_URL, $url );
 
-	private function setOptions( ) {
-		if( isset( $this->customOptions[ CURLOPT_HEADER ] ) && $this->customOptions[ CURLOPT_HEADER ] ) {
-			$this->addOption( CURLINFO_HEADER_OUT, true );
-		}
+        return $this->exec();
+    }
 
-		$options = $this->customOptions + $this->defaultOptions;
-		curl_setopt_array( $this->ch, $options );
-	}
+    private function setOptions() {
+        if( isset( $this->customOptions[ CURLOPT_HEADER ] ) && $this->customOptions[ CURLOPT_HEADER ] ) {
+            $this->addOption( CURLINFO_HEADER_OUT, true );
+        }
 
-	private function exec( ) {
-		$this->setOptions( );
-		$response = curl_exec( $this->ch );
+        $options = $this->customOptions + $this->defaultOptions;
+        curl_setopt_array( $this->ch, $options );
+    }
 
-		$this->data[ 'info' ] = curl_getinfo( $this->ch );
-		if( isset( $this->customOptions[ CURLOPT_HEADER ] ) && $this->customOptions[ CURLOPT_HEADER ] ) {
-			$this->data[ 'info' ][ 'request_header' ] = trim( $this->data[ 'info' ][ 'request_header' ] );
-			$this->processHeaders( $response );
-		}
+    private function exec() {
+        $this->setOptions();
+        $response = curl_exec( $this->ch );
 
-		curl_close( $this->ch );
+        $this->data[ 'info' ] = curl_getinfo( $this->ch );
+        if( isset( $this->customOptions[ CURLOPT_HEADER ] ) && $this->customOptions[ CURLOPT_HEADER ] ) {
+            $this->data[ 'info' ][ 'request_header' ] = trim( $this->data[ 'info' ][ 'request_header' ] );
+            $this->processHeaders( $response );
+        }
 
-		return $response;
-	}
+        curl_close( $this->ch );
 
-	private function processHeaders( &$data ) {
-		$tmp = explode( "\r\n\r\n", $data, 2 );
+        return $response;
+    }
 
-		$this->data[ 'info' ][ 'response_header' ] = $tmp[ 0 ];
-		$this->data[ 'info' ][ 'response_body' ] = $data = trim( $tmp[ 1 ] );
+    private function processHeaders( &$data ) {
+        $tmp = explode( "\r\n\r\n", $data, 2 );
 
-		$tmp = explode( "\r\n", $this->data[ 'info' ][ 'response_header' ] );
-		$this->data[ 'data' ][ 'Message' ] = $tmp[ 0 ];
-		for( $i = 1, $size = count( $tmp ); $i < $size; ++$i ) {
-			$string = explode( ': ', $tmp[ $i ], 2 );
-			$this->data[ 'data' ][ $string[ 0 ] ] = $string[ 1 ];
-		}
-	}
+        $this->data[ 'info' ][ 'response_header' ] = $tmp[ 0 ];
+        $this->data[ 'info' ][ 'response_body' ] = $data = trim( $tmp[ 1 ] );
 
-	private function getDataItem( $what, $name ) {
-		if( isset( $this->data[ $what ][ $name ] ) ) {
-			return $this->data[ $what ][ $name ];
-		}
-		else {
-			return null;
-		}
-	}
+        $tmp = explode( "\r\n", $this->data[ 'info' ][ 'response_header' ] );
+        $this->data[ 'data' ][ 'Message' ] = $tmp[ 0 ];
+        for( $i = 1, $size = count( $tmp ); $i < $size; ++$i ) {
+            $string = explode( ': ', $tmp[ $i ], 2 );
+            $this->data[ 'data' ][ $string[ 0 ] ] = $string[ 1 ];
+        }
+    }
+
+    private function getDataItem( $what, $name ) {
+        if( isset( $this->data[ $what ][ $name ] ) ) {
+            return $this->data[ $what ][ $name ];
+        }
+        else {
+            return null;
+        }
+    }
 }
--- modules/servers/onappusers/includes/php/getCP.php
+++ modules/servers/onappusers/includes/php/getCP.php
@@ -1,7 +1,7 @@
 <?php
 
-if( !isset( $_POST[ 'authenticity_token' ] ) ) {
-	exit( 'Don\'t allowed!' );
+if( ! isset( $_POST[ 'authenticity_token' ] ) ) {
+    exit( 'Don\'t allowed!' );
 }
 
 $root = dirname( dirname( dirname( dirname( dirname( dirname( $_SERVER[ 'SCRIPT_FILENAME' ] ) ) ) ) ) ) . DIRECTORY_SEPARATOR;
@@ -9,48 +9,48 @@ require $root . 'dbconnect.php';
 require $root . 'includes/functions.php';
 require $root . 'includes/clientareafunctions.php';
 
-$iv_size   = mcrypt_get_iv_size( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB );
-$iv        = mcrypt_create_iv( $iv_size, MCRYPT_RAND );
-$key       = substr( $_SESSION[ 'utk' ][ 0 ], 0, 27 );
+$iv_size = mcrypt_get_iv_size( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB );
+$iv = mcrypt_create_iv( $iv_size, MCRYPT_RAND );
+$key = substr( $_SESSION[ 'utk' ][ 0 ], 0, 27 );
 $crypttext = mcrypt_decrypt( MCRYPT_RIJNDAEL_256, $key, base64_decode( base64_decode( $_SESSION[ 'utk' ][ 1 ] ) ), MCRYPT_MODE_ECB, $iv );
-$data      = explode( '%%%', $crypttext );
+$data = explode( '%%%', $crypttext );
 
 if( count( $data ) != 2 ) {
-	exit( 'Corrupted data!' );
+    exit( 'Corrupted data!' );
 }
 else {
-	$data = json_decode( $data[ 0 ] );
+    $data = json_decode( $data[ 0 ] );
 }
 
 ?>
 
 <noscript>
-	<meta http-equiv="refresh" content="0; url=<?php echo $data->server ?>">
+    <meta http-equiv="refresh" content="0; url=<?php echo $data->server ?>">
 </noscript>
 <base href="<?php echo $data->server ?>">
 <div id="cpform" style="display: none;">
-<?php
-	require 'CURL.php';
-	$curl = new CURL;
-	$curl->addOption( CURLOPT_RETURNTRANSFER, true );
-	$curl->addOption( CURLOPT_FOLLOWLOCATION, true );
-	$cp = $curl->get( $data->server );
+    <?php
+    require 'CURL.php';
+    $curl = new CURL;
+    $curl->addOption( CURLOPT_RETURNTRANSFER, true );
+    $curl->addOption( CURLOPT_FOLLOWLOCATION, true );
+    $cp = $curl->get( $data->server );
 
-	if( $curl->getRequestInfo( 'http_code' ) == 200 ) {
-		$js = <<<JS
-			$( '#user_login' ).val( '{$data->login}' );
-			$( '#user_password' ).val( '{$data->password}' );
-			var form = $( 'form' );
-			form.attr( 'action', '{$data->server}/users/sign_in' );
-			form.attr( 'autocomplete', 'off' );
-			form.submit();
-			$( '#getcp' ).remove();
+    if( $curl->getRequestInfo( 'http_code' ) == 200 ) {
+        $js = <<<JS
+            $( '#user_login' ).val( '{$data->login}' );
+            $( '#user_password' ).val( '{$data->password}' );
+            var form = $( 'form' );
+            form.attr( 'action', '{$data->server}/users/sign_in' );
+            form.attr( 'autocomplete', 'off' );
+            form.submit();
+            $( '#getcp' ).remove();
 JS;
-		$js = '<script type="text/javascript" id="getcp">' . $js . '</script>';
-	}
-	else {
-		$js = '<script type="text/javascript">document.getElementById( "cpform" ).style.display = "block";</script>';
-	}
-	echo $cp . $js;
-?>
+        $js = '<script type="text/javascript" id="getcp">' . $js . '</script>';
+    }
+    else {
+        $js = '<script type="text/javascript">document.getElementById( "cpform" ).style.display = "block";</script>';
+    }
+    echo $cp . $js;
+    ?>
 </div>
