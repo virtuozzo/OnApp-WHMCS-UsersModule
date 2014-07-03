4077c490276c5d0c8b7a11c901b1ded79369e6a7
Lev Bartashevsky <n@ne>
2014-07-03 07:09:58 +0300

    reformat code

--- modules/servers/onappusers/module.sql
+++ modules/servers/onappusers/module.sql
@@ -1,11 +1,11 @@
 -- module tables
 -- Create syntax for table 'tblonappusers'
 CREATE TABLE IF NOT EXISTS `tblonappusers` (
-        `server_id`     INT(11) NOT NULL,
-        `client_id`     INT(11) NOT NULL,
-        `service_id`    INT(11) NOT NULL,
-        `onapp_user_id` INT(11) NOT NULL,
-        PRIMARY KEY (`server_id`, `client_id`, `service_id`),
-        KEY `client_id` (`client_id`)
+    `server_id`     INT(11) NOT NULL,
+    `client_id`     INT(11) NOT NULL,
+    `service_id`    INT(11) NOT NULL,
+    `onapp_user_id` INT(11) NOT NULL,
+    PRIMARY KEY (`server_id`, `client_id`, `service_id`),
+    KEY `client_id` (`client_id`)
 )
 ENGINE=InnoDB DEFAULT CHARSET=utf8;
